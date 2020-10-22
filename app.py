5950f6e98d558639fdf764ee9185611510b68516
powerthecoder <68101457+powerthecoder@users.noreply.github.com>
2020-10-21 23:12:40 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request
+import os
+import sys
+import json
+
+
+app = Flask('__main__')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1")
